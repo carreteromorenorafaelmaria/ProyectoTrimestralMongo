254e4cc7859f2bb81a9bb6b4450f6f6be515a4fc
carreteromorenorafaelmaria <72375022+carreteromorenorafaelmaria@users.noreply.github.com>
2020-11-16 09:02:23 +0100

    Update proyectofinaltrimestre.js

--- src/proyectofinaltrimestre.js
+++ src/proyectofinaltrimestre.js
@@ -143,15 +143,27 @@ db.futbolistaspremierleague.insertMany([
         
         
      ]);
-     db.futbolistaspremierleague.find() //Para que me muestre que todos los valores introducidos están bien.
-     db.futbolistaspremierleague.find({$or:[{"posiciones preferidas":"DC"},{"posiciones preferidas":"EI"}]}) //Find para que me de los jugadores que pueden jugar en las posiciones de EI(Extremo Izquierdo) o DC(Delantero Centro)
-     db.futbolistaspremierleague.find( { "pie preferido":{ $ne: "izquierdo" }, "valor de mercado": { $lte: 40000000 } } ) //Find que no son zurdos y su valor de mercado es menor o igual a 40M€
-     db.futbolistaspremierleague.find({"equipos": {$eq: "Manchester City" }})//Find que da los jugadores que han jugado o juegan en el Manchester City
-     db.futbolistaspremierleague.find( { "edad":{ $gt:25  }, "edad": { $lt: 29 } } )//Find que da los jugadores que están entre los 25 y 29 años
-     db.futbolistaspremierleague.find( { "pie preferido": null } ) //Find para los valores de pie preferido que son nulos
-     db.futbolistaspremierleague.find( { "NombreFutbolista": { $regex: /^R/i} } )// Find con regex de jugadores que empiezan por la R sin distinguir mayúsculas o minúsculas.
-     db.futbolistaspremierleague.find({"altura": {$gte: 1.78 }})// Find con jugadores que pasan el 1.78
-     db.futbolistaspremierleague.find( { "país": { $eq: "Inglaterra"} }) //Find para que salgan los jugadores ingleses
-     db.futbolistaspremierleague.find({"posiciones preferidas":{ $all:["EI","DC"]}})//Find de jugadores que pueden jugar de EI(Extremo Izquierdo) y de DC
-     db.futbolistaspremierleague.find({$nor:[{"posiciones preferidas":"ED"},{"posiciones preferidas":"MCO"}]})//Find de jugadores que no son ni ED(Extremo Derecho) ni MCO(Mediocentro ofensivo)
-     db.futbolistaspremierleague.find({ $and: [ { "valor de mercado": { $lte: 30000000 } },{$or:[{"país":"Escocia"},{"país":"Inglaterra"},{"país":"Gales"}]}  ] } )//Find con jugadores con valor de mercado menor a 30M€ y de las Islas Británicas.
+     //Para que me muestre que todos los valores introducidos están bien.
+     db.futbolistaspremierleague.find() 
+     //Find para que me de los jugadores que pueden jugar en las posiciones de EI(Extremo Izquierdo) o DC(Delantero Centro)
+     db.futbolistaspremierleague.find({$or:[{"posiciones preferidas":"DC"},{"posiciones preferidas":"EI"}]}) 
+     //Find que no son zurdos y su valor de mercado es menor o igual a 40M€
+     db.futbolistaspremierleague.find( { "pie preferido":{ $ne: "izquierdo" }, "valor de mercado": { $lte: 40000000 } } ) 
+     //Find que da los jugadores que han jugado o juegan en el Manchester City
+     db.futbolistaspremierleague.find({"equipos": {$eq: "Manchester City" }})
+     //Find que da los jugadores que están entre los 25 y 29 años
+     db.futbolistaspremierleague.find( { "edad":{ $gt:25  }, "edad": { $lt: 29 } } )
+     //Find para los valores de pie preferido que son nulos
+     db.futbolistaspremierleague.find( { "pie preferido": null } ) 
+     // Find con regex de jugadores que empiezan por la R sin distinguir mayúsculas o minúsculas.
+     db.futbolistaspremierleague.find( { "NombreFutbolista": { $regex: /^R/i} } )
+     // Find con jugadores que pasan el 1.78
+     db.futbolistaspremierleague.find({"altura": {$gte: 1.78 }})
+     //Find para que salgan los jugadores ingleses
+     db.futbolistaspremierleague.find( { "país": { $eq: "Inglaterra"} }) 
+     //Find de jugadores que pueden jugar de EI(Extremo Izquierdo) y de DC
+     db.futbolistaspremierleague.find({"posiciones preferidas":{ $all:["EI","DC"]}})
+     //Find de jugadores que no son ni ED(Extremo Derecho) ni MCO(Mediocentro ofensivo)
+     db.futbolistaspremierleague.find({$nor:[{"posiciones preferidas":"ED"},{"posiciones preferidas":"MCO"}]})
+     //Find con jugadores con valor de mercado menor a 30M€ y de las Islas Británicas.
+     db.futbolistaspremierleague.find({ $and: [ { "valor de mercado": { $lte: 30000000 } },{$or:[{"país":"Escocia"},{"país":"Inglaterra"},{"país":"Gales"}]}  ] } )
